439c893821cd4df10acda2b8586bd56c26393728
kuntabruno <kuntabruno@gmail.com>
2022-01-15 10:35:13 +0300

    Included model in products module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsController } from './controllers/product.controller';
+import { Product, ProductSchema } from './models/product.model';
+import { ProductService } from './services/product.services';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+  ],
+  controllers: [ProductsController],
+  providers: [ProductService],
+})
 export class ProductsModule {}
